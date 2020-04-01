4bc781269ce7ed202e460658485a8dca1fbe9e3b
Osas <omfaluyi@gmail.com>
2020-04-01 16:28:59 +0100

    Improved error handling for User.Signin created getUserSnapshot function to remove duplicated code in SignIn and Edit Prettified some errors Added Edit Function Created an Error Type(authError) Added more robust error handling to Init Function Adjusted authMiddleware signature and removed session token from requirements Split up app into smaller files Removed Items and Totals Moved Flash to a seperate package Commented most functions Added DeleteDeadSessions function Unexported randomString()

--- auth.go
+++ auth.go
@@ -1,557 +1,147 @@
-// auth handles session/token based authentication on firebase
+// auth handles session/token based authentication on firebase DataStore
 // to initialise package an InitAuthVariables struct must be created and the initialized
 // in main
+// Required Collections
+// 1. "users" stores user data.
+//		Required Fields:
+//			*"email": must be unique //future: accommodate non-uniqueness
+//			*"password": contains MD5 hash of the password
+// 2. "sessions" stores session data.
+//		Required Fields:
+//			*"email"
+//			*"expiryDate": when the session is no longer considered valid
+//				you can run a cron to sweep through and delete sessions
+//				with an expiryDate before time.Now() or use the
+//				DeleteDeadSessions() function
+//			*"role": the session user's role
 package auth
 
 import (
 	"cloud.google.com/go/firestore"
 	"context"
-	"encoding/base64"
-	"errors"
 	"fmt"
-	"golang.org/x/crypto/bcrypt"
-	"google.golang.org/grpc/status"
-	"html/template"
-	"math/rand"
 	"net/http"
-	"strconv"
-	"strings"
-	"time"
 )
+
+const (
+	ErrNoEmail          = 1  // No Email
+	ErrNoPassword       = 2  // No Password
+	ErrInitVars         = 3  // Initialization variable error
+	ErrNoUser           = 4  // User does not exist
+	ErrDuplicateUser    = 5  // Two users exist with email
+	ErrParseError       = 6  // Parse Error
+	ErrWrongPassword    = 7  // Wrong Password
+	ErrNoSessionCreated = 9  // Couldn't create a session
+	ErrUserNotApproved  = 10 // User has not been approved yet
+	ErrNoSession        = 11 // Session doesn't exist
+)
+
+var av *InitAuthVariables
 
 // InitAuthVariables is a holder for the auth initialization variables
 type InitAuthVariables struct {
-	CookieName          string            // used to store user sessions
-	FlashCookieName     string            // Used to display flashes to the user
-	DBName              *firestore.Client // Firestore client connection
-	RedirectOnSignIn    string            // Route user is redirected to, on Sign in
-	redirectIfLoggedout string            // Route user is redirected to, on Logout
-	redirectIfNoRight   string            // Route user is redirected to, if they lack permission
-	SessionLife         string            //secondsst, string
-	GcloudCtx           context.Context   // gcloud context
+	CookieName        string            // *Required* used to store user sessions
+	FlashCookieName   string            // *Required* Used to display flashes to the user
+	DBName            *firestore.Client // *Required* Firestore client connection
+	RedirectOnSignIn  string            // *Required* Route user is redirected to, on Sign in
+	RedirectOnLogOut  string            // *Required* Route user is redirected to, on Logout
+	redirectIfNoRight string            // *Required* Route user is redirected to, if they lack permission
+	SessionLife       int               // *Required* How long a session should live on the server (seconds)
+	GCContext         context.Context   // *Required* GCloud context
 }
-
-var (
-	Av          InitAuthVariables
-	sessionLife int //secondsst, string
-	err1        error
-)
 
 // Init initializes the package based on variables defined in the
 // InitAuthVariables struct
-func (iav InitAuthVariables) Init() {
-	Av = iav
-	sessionLife, err1 = strconv.Atoi(iav.SessionLife) // SessionLife
-	if err1 != nil {
-		fmt.Println("Error getting session life", err1)
+func (iav *InitAuthVariables) Init() error {
+	var err string
+	if iav.CookieName == "" {
+		err = "no name provided for session cookie"
+	} else if iav.FlashCookieName == "" {
+		err = "no name provided for flash cookie"
+	} else if iav.DBName == nil {
+		err = "no firestore client provided"
+	} else if iav.SessionLife == 0 {
+		err = "session life not set"
+	} else if iav.GCContext == nil {
+		err = "no GC context provided"
 	}
-}
 
-// Session represents a user session stored on the database
-type Session struct {
-	Email      string    //`firestore:"email"`
-	ExpiryDate time.Time //`firestore:"expireDate"`
-	FirstName  string    //`firestore:"firstName"`
-	LastName   string    //`firestore:"lastName"`
-	Role       string    //`firestore:"roles"`
-}
-
-// CanAccess checks if a user's role is one of those listed
-// in the roles array. This is used in tandem with the authMiddleware
-// function to restrict access to certain handlers
-func (s *Session) CanAccess(roles []string) bool {
-	for _, x := range roles {
-		if s.Role == x {
-			return true
+	if err != "" {
+		return &authError{
+			msg:     err,
+			errType: ErrInitVars,
 		}
 	}
 
-	return false
+	av = iav
+	return nil
 }
 
-type User struct {
-	FirstName string `json:"FirstName" firestore:"firstName"`
-	LastName  string `firestore:"lastName"`
-	Email     string `firestore:"email"`
-	Password  string `firestore:"password"`
-	UserID    int
-	Role      string `firestore:"role"`
-	Approved  bool   `firestore:"approved"`
+// authError handles errors for this package
+type authError struct {
+	msg      string
+	errType  int
+	ancestor error
 }
 
-// GetSessionUser uses the GetSessionDetails function to
-// ...get the session details, and parses the returned session
-// to a User struct
-func (u *User) GetSessionUser(s string) {
-	m, err := GetSessionDetails(s)
-	if err != nil {
-		fmt.Println("Error getting session user")
-		return
-	}
-	u.FirstName = m.FirstName
-	u.LastName = m.LastName
-	u.Email = m.Email
+// Error implements the error interface
+func (sE *authError) Error() string {
+	return fmt.Sprintf(sE.msg)
 }
 
-// SignIn confirms that the entered User credentials (email and password)
-// match what is in the Firestore, creates a session for the user on the server,
-// and returns a cookie containing the session token
-func (u User) SignIn() (*http.Cookie, error) {
-	pw := u.Password
+// AuthMiddleware keeps unauthorized users from accessing the provided handler
+// next: is the destination handler
+// roles: is a list of the roles authorized to access handler
+// rdr: is the route user is redirected to if they lack access rights
+func AuthMiddleware(next func(w http.ResponseWriter, r *http.Request),
+	roles []string, rdr string) http.Handler {
 
-	// check for user
-	usr, err := Av.DBName.Collection("users").Where("email", "==", u.Email).
-		Where("approved", "==", true).Documents(Av.GcloudCtx).GetAll()
-	if err != nil {
-		return nil, errors.New("user " + u.Email + " does not exist")
-	}
-
-	err = usr[0].DataTo(&u)
-	if err != nil {
-		return nil, errors.New("error parsing user details to user struct: " + err.Error())
-	}
-
-	//compare password
-	err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(pw))
-	if err != nil {
-		return nil, errors.New("wrong password")
-	}
-
-	// create session
-	c, err := CreateSession(u, Av.CookieName, sessionLife)
-	if err != nil {
-		fmt.Println(err)
-		return nil, errors.New("Login Error, Contact Admin")
-	}
-
-	return c, nil
-}
-
-//func (u *User) assignUserRoles() error {
-//
-//	resp, err := Av.DBName.Query(
-//		`SELECT role_id FROM many_users_have_many_roles
-//				WHERE email = $1`, u.Email)
-//	if err != nil {
-//		return err
-//	}
-//
-//	for resp.Next() {
-//		var i int
-//		resp.Scan(&i)
-//		u.Roles = append(u.Roles, i)
-//	}
-//	return nil
-//}
-
-type itemAndTotals struct {
-	Items interface{}
-	Total int
-}
-
-type PageVariables struct {
-	BadFlash       string
-	GoodFlash      string
-	Data           interface{}
-	Data2          interface{}
-	ItemsAndTotals itemAndTotals
-	User           User
-	Title          string
-	CompanyName    string
-	TopFlash       struct {
-		Bad  string
-		Good string
-	}
-}
-
-// AddFlash Adds a flash to the pagevariables struct to be rendered immediately
-// Overrides any flash already in the template
-func (pv *PageVariables) AddFlash(f string) {
-
-	if strings.HasPrefix(f, "Warning:") {
-		ns := strings.TrimLeft(f, "Warning:")
-		pv.BadFlash = ns
+	var rdr2 string
+	if rdr == "" {
+		rdr2 = av.RedirectOnLogOut
 	} else {
-		pv.GoodFlash = f
+		rdr2 = rdr
 	}
 
-}
-
-// 13. Create New User
-func CreateUser(u User) error {
-
-	//Check if user already exists
-	usrs := Av.DBName.Collection("users").Where("email", "==", u.Email).Documents(Av.GcloudCtx)
-	for {
-		_, err := usrs.Next()
-		if err != nil {
-			break
-		}
-		if err == nil { // if username is found, return a username found error
-			return errors.New("User already exists")
-		}
-	}
-
-	// 1. Generate Password hash
-	pass, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return errors.New("Password Encryption failed" + err.Error())
-	}
-
-	u.Password = string(pass)
-
-	// 2. Add User
-	_, _, err = Av.DBName.Collection("users").Add(Av.GcloudCtx, u)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-
-}
-
-// Sign In
-func SignIn(w http.ResponseWriter, r *http.Request) {
-	// if GET, Check if a session already exists...
-	if r.Method != http.MethodPost {
-		c, err := r.Cookie(Av.CookieName)
-		if err == nil {
-			_, done2 := isUserNameInCache(c.Value, w, r)
-			// .. if session exists redirect to dashboard
-			if done2 {
-				http.Redirect(w, r, Av.RedirectOnSignIn, http.StatusFound)
-				return
-			}
-		}
-		// ... if session doesn't exist render login page
-		pv := PageVariables{Title: "788 Kollektor"}
-		pv.AddFlash(GetFlash(w, r, Av.CookieName))
-		t := template.Must(template.ParseFiles("templates/a.html",
-			"templates/signin.html"))
-		err = t.Execute(w, pv)
-		if err != nil {
-			fmt.Println("Couldn't parse template", err)
-			return
-		}
-		return
-	}
-
-	// If post...
-	r.ParseForm()
-	u := User{Email: r.FormValue("Email"), Password: r.FormValue("Password")}
-	// create session
-	c, err := u.SignIn()
-	if err != nil {
-		fmt.Println("Error finding user:", err)
-		SetFlash(w, "Warning:Server error code zambezi")
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-	//Set Cookie
-	http.SetCookie(w, c)
-	fmt.Println("Logged In Successfully")
-	http.Redirect(w, r, Av.RedirectOnSignIn, http.StatusFound)
-	return
-}
-
-func CreateSession(user User, ckName string, life int) (*http.Cookie, error) {
-	////  Generate a token
-	sessionToken := RandomString(45)
-	expiryDate := time.Now().Add(time.Duration(life) * time.Second)
-
-	//// Write to DB
-	_, err := Av.DBName.Collection("sessions").Doc(sessionToken).
-		Set(Av.GcloudCtx, map[string]interface{}{
-			"email":      user.Email,
-			"firstName":  user.FirstName,
-			"lastName":   user.LastName,
-			"role":       user.Role,
-			"expiryDate": expiryDate,
-		})
-
-	if err != nil {
-		return nil, err
-	}
-
-	// Set Cookie
-	return &http.Cookie{
-		Name:    ckName,
-		Value:   sessionToken,
-		Expires: expiryDate,
-	}, nil
-}
-
-func SignOut(w http.ResponseWriter, r *http.Request) {
-	c, err := r.Cookie(Av.CookieName)
-	if err != nil {
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusUnauthorized)
-			fmt.Println("Client didn't send a cookie named:", Av.CookieName, err)
-			return
-		}
-		fmt.Println("Didn't work for other reason:", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	sessionToken := c.Value
-
-	// NUNU
-	if err = KillSession(sessionToken); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Println("Couldn't kill Session from server", err)
-		return
-	}
-
-	// Wipe client cookie
-	http.SetCookie(w, &http.Cookie{Name: Av.CookieName, Value: ""})
-	http.Redirect(w, r, "/", http.StatusFound)
-	return
-}
-
-func KillSession(sessionToken string) error {
-	_, err := Av.DBName.Collection("sessions").Doc(sessionToken).Delete(Av.GcloudCtx)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func AuthMiddleware(next func(w http.ResponseWriter, r *http.Request, userToken string), roles []string) http.Handler {
-
-	/*
-			      todo adjust response based on conditions
-		1. Session doesn't exist, redirect to log in page
-			2. Sesion exists but role does not, redirect to no access page
-	*/
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 
-			c, err := r.Cookie(Av.CookieName)
+			// 1. Get cookie
+			c, err := r.Cookie(av.CookieName)
 			if err != nil {
 				if err == http.ErrNoCookie {
 					fmt.Println("Client didn't send a cookie:", err)
-					http.Redirect(w, r, "/", http.StatusUnauthorized)
+					//todo: handle better
+					http.Redirect(w, r, rdr2, http.StatusUnauthorized)
 					return
 				}
 				fmt.Println("Didn't work for other reason:", err)
-				http.Redirect(w, r, "/", http.StatusUnauthorized)
+				//todo: handle better
+				http.Redirect(w, r, rdr2, http.StatusUnauthorized)
 				return
 			}
+			// 1.b. get session token from cookie
 			sessionToken := c.Value
-			err, done2 := isUserNameInCache(sessionToken, w, r)
-			if !done2 {
-				fmt.Println("session search error:", err)
-				http.Redirect(w, r, "/", http.StatusUnauthorized)
-				return
-			}
 
-			// ROLES
-			// determine User role
-			s, err := GetSessionDetails(sessionToken)
+			// 2. Get session details from FireStore
+			s, err := GetSession(sessionToken)
 			if err != nil {
-				fmt.Println("error if any:", err)
-				http.Redirect(w, r, "/", http.StatusForbidden)
+				fmt.Println("error getting session details", err)
+				//todo: handle better
+				http.Redirect(w, r, rdr2, http.StatusForbidden)
 				return
 			}
 
-			//2. Learn how to store the User role in redis during sign in and pull it from there on check*/
-			// check if it exists in role
+			// 3. Check if user has access
 			if s.CanAccess(roles) != true {
 				fmt.Println("User does not have the necessary privileges to view this page")
-				w.WriteHeader(http.StatusForbidden)
+				//todo: handle better
+				http.Redirect(w, r, r.Header.Get("Referer"), http.StatusForbidden)
 				return
 			}
 
-			next(w, r, sessionToken)
+			next(w, r)
 
 		})
 
-}
-
-func isUserNameInCache(sessionToken string, w http.ResponseWriter, r *http.Request) (error, bool) {
-
-	//1. Check if it's in the cache
-	s, err := Av.DBName.Collection("sessions").Doc(sessionToken).
-		Get(Av.GcloudCtx)
-	if err != nil {
-		return errors.New("error querying db for session: " + status.Code(err).String()), false
-	}
-	// return false if session doesn't exist
-	if s.Exists() == false {
-		return errors.New("no Session with that token"), false
-	}
-
-	var ss Session
-	// delete if it has expired
-	if err := s.DataTo(&ss); err != nil {
-		return errors.New("couldn't parse session to struct"), false
-	}
-	if ss.ExpiryDate.Before(time.Now()) {
-		// Delete session
-		_, err := s.Ref.Delete(Av.GcloudCtx)
-		if err != nil {
-			return errors.New("session expired, failed to delete"), false
-		}
-		return errors.New("session expired, deleted"), false
-	}
-	return nil, true
-}
-
-func SetFlash(w http.ResponseWriter, value string) {
-	c := &http.Cookie{Name: Av.FlashCookieName,
-		Value: base64.URLEncoding.EncodeToString([]byte(value))}
-	http.SetCookie(w, c)
-}
-
-func GetFlash(w http.ResponseWriter, r *http.Request, name string) string {
-
-	c, err := r.Cookie(name)
-	if err != nil {
-		switch err {
-		case http.ErrNoCookie:
-			return ""
-		default:
-			fmt.Println("Error getting flash cookie")
-			return "Internal Error 106"
-		}
-	}
-
-	value, err := base64.URLEncoding.DecodeString(c.Value)
-	if err != nil {
-		fmt.Println("Error decoding flash cookie value")
-		return "Internal Error 107"
-	}
-	dc := &http.Cookie{Name: name, MaxAge: -1, Expires: time.Unix(1, 0)}
-	http.SetCookie(w, dc)
-	return string(value)
-}
-
-func randomInt(min, max int) int {
-	return min + rand.Intn(max-min)
-}
-
-// Generate a random string of A-Z chars with len = l
-func RandomString(len int) string {
-	bytes := make([]byte, len)
-	for i := 0; i < len; i++ {
-		bytes[i] = byte(randomInt(65, 90))
-	}
-	return string(bytes)
-}
-
-// GetSessionDetails checks the database to see if
-// there is a valid session with the provided token (s)
-// and returns the session if so
-func GetSessionDetails(sT string) (Session, error) {
-
-	s, err := Av.DBName.Collection("sessions").Doc(sT).Get(Av.GcloudCtx)
-	if err != nil {
-		return Session{}, errors.New("error querying db for Session: " + status.Code(err).String())
-	}
-
-	if s.Exists() != true {
-		return Session{}, errors.New("Session doesn't exist: ")
-	}
-
-	var se Session
-	err = s.DataTo(&se)
-	if err != nil {
-		return Session{}, errors.New("error parsing Session to struct: ")
-	}
-	return se, nil
-}
-
-//Deprecated Functions
-func convertStringSlicetoInterface(ss []string) []interface{} {
-
-	s := make([]interface{}, len(ss))
-	for i, v := range ss {
-		s[i] = v
-	}
-
-	return s
-}
-func getKeysMatchingPattern(p string) ([]string, error) {
-
-	//c := "0"
-	var kk []string
-	//
-	//resp, err := redis.Values(Cache.Do("SCAN", c, "MATCH", p+"*"))
-	//if err != nil {
-	//	fmt.Println("Error doing scan1", err)
-	//	return nil, err
-	//}
-	//if resp[1] != nil {
-	//	kk = convertbyteSlicetoStringlice(resp[1].([]interface{}))
-	//}
-	//
-	//c = string(resp[0].([]uint8))
-	//
-	//for c != "0" {
-	//
-	//	resp, err := redis.Values(Cache.Do("SCAN", c, "MATCH", p+"*"))
-	//	if err != nil {
-	//		fmt.Println("Error doing scan2", err)
-	//		return nil, err
-	//	}
-	//	if resp[1] != nil {
-	//		kk = append(kk, convertbyteSlicetoStringlice(resp[1].([]interface{}))...)
-	//	}
-	//	c = string(resp[0].([]uint8))
-	//}
-
-	return kk, nil
-}
-func DoesXHaveY(x, y []string) bool {
-
-	var ct int
-
-	for n, a := range y {
-		for _, b := range x {
-			if a == b {
-				ct++
-				break
-			}
-		}
-		if ct != n+1 {
-			return false
-		}
-	}
-
-	if ct == len(y) {
-		return true
-	}
-
-	return false
-
-}
-func convertbyteSlicetoStringlice(bs []interface{}) []string {
-	var ss []string
-	for _, x := range bs {
-		ss = append(ss, string(x.([]byte)))
-	}
-	return ss
-}
-func pushRoles(rr []int, k string) error {
-	//	for _, x := range rr {
-	//		_, err := Cache.Do("RPUSH", k, strconv.Itoa(x)) //roles
-	//		if err != nil {
-	//			return err
-	//		}
-	//	}
-	return nil
-}
-func convertRolestoInt(rs []string) ([]int, error) {
-	var roles []int
-	//	for _, r := range rs {
-	//		ri, err := strconv.Atoi(r)
-	//		if err != nil {
-	//			return nil, err
-	//		}
-	//		roles = append(roles, ri)
-	//	}
-	return roles, nil
 }
---
+++ session.go
@@ -0,0 +1,146 @@
+package auth
+
+import (
+	"errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+// Session represents a user session stored on the database
+type Session struct {
+	Email      string    //`firestore:"email"`
+	ExpiryDate time.Time //`firestore:"expiryDate"`
+	FirstName  string    //`firestore:"firstName"`
+	LastName   string    //`firestore:"lastName"`
+	Role       string    //`firestore:"roles"`
+}
+
+// CanAccess checks if a user's role is one of those listed
+// in the roles slice. This is used in tandem with the authMiddleware
+// function to restrict access to certain handlers
+func (s *Session) CanAccess(roles []string) bool {
+	for _, x := range roles {
+		if s.Role == x {
+			return true
+		}
+	}
+	return false
+}
+
+// CreateSession creates a new session, writes this to the DB and returns a cookie
+func CreateSession(user *User, ckName string, life int) (*http.Cookie, error) {
+	////  Generate a token
+	sessionToken := randomString(45)
+	expiryDate := time.Now().Add(time.Duration(life) * time.Second)
+
+	//// Write to DB
+	_, err := av.DBName.Collection("sessions").Doc(sessionToken).
+		Set(av.GCContext, map[string]interface{}{
+			"email":      user.Email,
+			"firstName":  user.FirstName,
+			"lastName":   user.LastName,
+			"role":       user.Role,
+			"expiryDate": expiryDate,
+		})
+
+	if err != nil {
+		return nil, &authError{
+			msg:     "error creating session: " + err.Error(),
+			errType: ErrNoSessionCreated,
+		}
+	}
+
+	// Set Cookie
+	return &http.Cookie{
+		Name:    ckName,
+		Value:   sessionToken,
+		Expires: expiryDate,
+	}, nil
+}
+
+// KillSession deletes a session from the database
+func KillSession(sessionToken string) error {
+	_, err := av.DBName.Collection("sessions").Doc(sessionToken).Delete(av.GCContext)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// randomInt generates a random integer between min and max
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+// randomString generates a random string of A-Z chars with len = l
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(65, 90))
+	}
+	return string(bytes)
+}
+
+// GetSession checks the database to see if
+// there is a valid session with the provided token (s)
+// and returns the session if so
+func GetSession(sT string) (*Session, error) {
+	// Dev Note: A valid session and an error must never be returned together
+	// There can be only one!
+
+	//1. Get session
+	s, err := av.DBName.Collection("sessions").Doc(sT).Get(av.GCContext)
+	if err != nil {
+		if status.Code(err) == codes.NotFound { // if session doesn't exist
+			return nil, &authError{
+				msg:      "session doesn't exist",
+				errType:  ErrNoSession,
+				ancestor: err,
+			}
+		} else { // if any other kind of error is returned
+			return nil, err
+		}
+	}
+
+	// parse session to struct
+	var se *Session
+	err = s.DataTo(&se)
+	if err != nil {
+		return nil, errors.New("error parsing Session to struct: ")
+	}
+
+	// Check if session has expired
+	if se.ExpiryDate.Before(time.Now()) {
+		// Delete session
+		_, err := s.Ref.Delete(av.GCContext)
+		if err != nil {
+			return nil, errors.New("session expired, failed to delete")
+		}
+		return nil, errors.New("session expired, deleted")
+	}
+
+	return se, nil
+}
+
+// DeleteDeadSessions deletes all the expired sessions from the FireStore DB
+func DeleteDeadSessions() error {
+	// get all dead sessions
+	q := av.DBName.Collection("sessions").
+		Where("expiryDate", "<", time.Now()).Documents(av.GCContext)
+	// delete them
+	x, err := q.GetAll()
+	if err != nil {
+		return err
+	}
+	for _, ds := range x {
+		_, err = ds.Ref.Delete(av.GCContext)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
